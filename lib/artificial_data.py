1cf62fccf1aaa504bc4b5fe78033eaa966265a56
Quentin FERRE <quentin.ferre@laposte.net>
2020-02-25 13:54:55 +0100

    Artificial data cleanup

--- lib/artificial_data.py
+++ lib/artificial_data.py
@@ -18,7 +18,6 @@ The goal is to show that the model will usually rebuilt the correlation group as
 
 
 
-
 def generator_fake(batch_size = 10,region_length = 160, #reliable_datasets=np.arange(16),
                     nb_datasets = 16, nb_tfs = 10, squish_factor = 10, ones_only = False,
                     watermark_prob = 0.75, tfgroup_split = 2/3, overlapping_groups = False, crumb = None):
@@ -50,14 +49,12 @@ def generator_fake(batch_size = 10,region_length = 160, #reliable_datasets=np.ar
             batch_status.append('data')
 
 
-        """
-        # Sometimes, give a matrix of only noise and tell it to rebuild it as "full zero"
+        # # Sometimes, give a matrix of only noise and tell it to rebuild it as "full zero"
         # if rand < p :
         #     noisy_shape = dense_matrix.shape
         #     noisy = make_a_fake_matrix(region_length,nb_datasets,nb_tfs,signal = False)
         #     batch_features.append('noisy')
         #     batch_status.append('noise')
-        """
 
 
         result = np.array(batch_features)
@@ -70,10 +67,7 @@ def generator_fake(batch_size = 10,region_length = 160, #reliable_datasets=np.ar
         target = np.array(target)
 
 
-
-
         # Squishing along the X axis to reduce computing cost
-
         result = [utils.squish(m, squish_factor) for m in result]
         target = [utils.squish(m, squish_factor) for m in target]
         result = np.array(result)
@@ -86,21 +80,13 @@ def generator_fake(batch_size = 10,region_length = 160, #reliable_datasets=np.ar
 
 
 
-
-
-
-
-
-
-
 def list_of_peaks_to_matrix(peaks,region_length,nb_datasets,nb_tfs, crumb=False, ones_only = False):
     """
     Utility function designed for make_a_fake_matrix(). It will convert a list of peak objects, which
     are tuples of (dataset, tf, center, length, intensity), into a 3D tensor.
     """
 
-    mat = np.zeros((region_length,nb_datasets,nb_tfs))
-    # TODO make this a sparse matrix ? Is that really necessary ?
+    mat = np.zeros((region_length,nb_datasets,nb_tfs)) # TODO make this a scipy.sparse matrix ? Is that really necessary ?
 
     # Now write the peaks to the matrix
     for peak in peaks:
@@ -111,11 +97,8 @@ def list_of_peaks_to_matrix(peaks,region_length,nb_datasets,nb_tfs, crumb=False,
         end = min(region_length, center+length)
 
         ## Write to matrix
-        # WARNING intensity must be between 0 and 1 !
-        # Or must it ?
-        # Let's clip at 0-1000 and divide by 1000
+        # Intensity is clipped at 0-1000 and divided by 1000
         intensity_clipped = np.clip(intensity,0,1000) / 1000
-
 
         # Write peak
         mat[begin:end,dataset,tf] = mat[begin:end,dataset,tf] + intensity_clipped
@@ -124,7 +107,6 @@ def list_of_peaks_to_matrix(peaks,region_length,nb_datasets,nb_tfs, crumb=False,
         if crumb :
             mat[begin:end,:,tf] = mat[begin:end,:,tf] + 0.1*intensity_clipped
             mat[begin:end,dataset,:] = mat[begin:end,dataset,:] + 0.1*intensity_clipped
-
 
         # Override : if ones_only, we overwrite the current value with 1
         if ones_only : mat[begin:end,dataset,tf] = 1
@@ -139,18 +121,12 @@ def list_of_peaks_to_matrix(peaks,region_length,nb_datasets,nb_tfs, crumb=False,
 
 
 
-
-
-
-
-
-
-def make_a_fake_matrix(region_length,nb_datasets,nb_tfs, reliable_datasets = None,
-                        signal = True, noise=True,
-                        ones_only = False, watermark_prob = 1, tfgroup_split = 2/3, overlapping_groups = False,
+def make_a_fake_matrix(region_length, nb_datasets, nb_tfs,
+                        reliable_datasets = None, signal = True, noise = True,
+                        ones_only = False, watermark_prob = 1,
+                        tfgroup_split = 2/3, overlapping_groups = False,
                         return_separately = False):
     """
-
     region_length,nb_datasets,nb_tfs : matrix size
     reliable_datasets : which datasets are reliable
     signal, noise : which kinds of peaks to genrate
@@ -158,7 +134,6 @@ def make_a_fake_matrix(region_length,nb_datasets,nb_tfs, reliable_datasets = Non
 
     return_separately : if True, will stop and return a separate list of lists of peaks (resp. stack, noise, watermark)
     to be processed in evaluation - this will NOT return a tensor.
-
     """
 
     # TODO Do not hardcode the parameters of Poisson and other laws
@@ -170,9 +145,6 @@ def make_a_fake_matrix(region_length,nb_datasets,nb_tfs, reliable_datasets = Non
     ###### DATASETS STEP
     # Choose datasets that will correlate
 
-    # TODO : remove the +1 for k and treat the k=0 case by skipping all of this until noise
-    # same for N
-
     # Using only "reliable datasets" this time (ie the first half, for example)
 
     if reliable_datasets == None :
@@ -180,84 +152,24 @@ def make_a_fake_matrix(region_length,nb_datasets,nb_tfs, reliable_datasets = Non
 
     k = ss.poisson(1).rvs() + 1
     datasets = np.random.choice(reliable_datasets,k)
-    # TODO : not all datasets correlate ! assign different probas ? Or simply draw from reliable_datasets
-    # datasets = np.random.choice(reliable_datasets,k)
+    # TODO : not all datasets correlate ! assign different probas ?
+    # TODO : weighted choice here to test intra group bias
 
-
-    """
-    TODO : weighted choice here ! to confirm my theory that intra group abundance biais is linear with abundance !
-    """
-
-
-
+    # Pick common center for the stack
     common_center = int(ss.uniform(0,region_length).rvs())
 
     # In some cases you may not want to write true peaks and produce only noise, you should set signal to false in this case
     if signal :
 
-
-        # Checkup : we want to pick from either the first half of tfs or the second half.
+        # We want to pick from either the first half of tfs or the second half.
         # The idea is that, when rebuilding, if the peak is from a top half TFs, the phantom should
         # come only from a top half tf since they are the ones correlating.
         tf_first_half = range(int(nb_tfs/2))
         tf_second_half = range(int(nb_tfs/2),nb_tfs)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        """
-        ANOTHER THING TO TEST A THEORY : OVERLAPPING groups
-        Pick not from first half OR second half. Instead pick from first+second OR First only.
-        Will help test my theory aout corr group size and overlapping groups
-        AGAIN TODO MAKE THIS A PARAMETER.
-        LEAVE IT OFF BY DEFAULT
-        """
-        #overlapping_groups = False # SHOULD BE FALSE BY DEFAULT, AND NOW IT IS OKAY
+        # Overlapping groups : If desired, pick not from first half OR second half
+        # but instead pick from first+second OR First only
         if overlapping_groups:
             tf_first_half = range(nb_tfs)
             tf_second_half = range(int(nb_tfs/2))
@@ -266,52 +178,12 @@ def make_a_fake_matrix(region_length,nb_datasets,nb_tfs, reliable_datasets = Non
             tf_second_half = range(int(nb_tfs/2),nb_tfs)
 
 
-
-
-
-
-
-        """
-        # Now, when we pick the correlating tfs, pick them from either the first half of the second half (50% chance)
-        # Now 2/3 - 1/3 chance to test a theory
-        """
-        #tfgroup_split = 2/3
-        # OKAY I PUT IT AT 2/3 BY DEFAULT
-        #tfgroup_split = 1/2
-        """
-        # TODO JUST MAKE THIS A PARAMETER !!!!!!!!!
-        # COPY ALL CASES WHERE I ADDED WATEMARK_PROB !!!!!!
-        """
-
-
-
-
-
-
-
-
-
+        # Choosing probability is a parameter (tfgroup_split)
         cointoss = random.uniform(0,1)
         cointoss = cointoss < tfgroup_split
 
         if cointoss : tfs_to_choose_from = tf_first_half
         else : tfs_to_choose_from = tf_second_half
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
         # Pick number of peaks to write
@@ -326,24 +198,19 @@ def make_a_fake_matrix(region_length,nb_datasets,nb_tfs, reliable_datasets = Non
             for tf in tfs:
                 center_for_this_tf = int(common_center + ss.uniform(-200,200).rvs())
                 length = int(ss.lognorm(scale=250,s=0.25).rvs())
-                intensity =  ss.norm(500,120).rvs() + ss.norm(10,10).rvs() # intenity = random + noise
+                intensity =  ss.norm(500,120).rvs() + ss.norm(10,10).rvs() # intensity = random + noise
                 # NOTE intensity is dicarded by default for now due to ones_only
 
                 # From analysis, peaks FOR THE SAME TF across different datasets should be very similar.
                 for dataset in datasets :
-                    """
                     # TODO Maybe instead we could pick here the 1+ datasets which have this TF.
                     # This way the TFs would not be replicated across always the same datasets.
-                    """
                     peaks_stack.append((dataset, tf, center_for_this_tf, length, intensity))
 
 
 
-    """
     # In some cases you may not want noise (ie. for pre-training)
     # TODO Split stack and nosie into their own functions
-    """
-
 
     ## NOISE STEP
     if noise :
@@ -370,21 +237,11 @@ def make_a_fake_matrix(region_length,nb_datasets,nb_tfs, reliable_datasets = Non
 
 
 
-
     # CONTROL BAR : have one of the datasets/TF pair have a peak on all the length
-    # of the region. The idea is that it will be very frequent but not particularly
-    # correlated with anything. Given how MSE works, it will still be rebuilt, and
-    # always (likely due to having its own filters).
-
-    # To prevent it form being too trivial, and from falling into a learning trap of
-    # rebuilding the watermak only when too many dimensions, don't make it always
-    # appear.
+    # of the region. It should get in most cases its own group or a slight correlation.
+    # To prevent learning traps, don't make it always appear.
     if ss.uniform.rvs() < watermark_prob:
         peaks_watermark.append((0,0,int(region_length/2),region_length-2,1000))
-
-
-
-
 
     # If we want to return separately (useful in evaluation, when computing scores per category of peak)
     if return_separately:
@@ -396,10 +253,9 @@ def make_a_fake_matrix(region_length,nb_datasets,nb_tfs, reliable_datasets = Non
 
 
 
-
-
-    # TODO NOTE : For now artificial data has no crumbs. Must allow it to be changed here. Less useful in artificial because the dimensions and
-    # sparsity are usually smaller in proof-of-concetps.
+    # TODO NOTE : For now artificial data has no crumbs.
+    # Must allow it to be changed here. Less useful in artificial because the
+    # dimensions and sparsity are usually smaller in proof-of-concetps.
 
     # Convert list of peaks to tensor
     result = list_of_peaks_to_matrix(peaks,region_length,nb_datasets,nb_tfs, crumb = False, ones_only = ones_only)
@@ -409,5 +265,4 @@ def make_a_fake_matrix(region_length,nb_datasets,nb_tfs, reliable_datasets = Non
 
     return result
     # TODO : make it possible to return (matrix) or (matrix, matrix_without_noise)
-
-    # TODO Those matrices are too dense need to revise down number of parameters
+    # TODO Those matrices are quite denser than typical real ones, may need to revise down number of parameters
